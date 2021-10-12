b95cc5845e35113bf2dc7ba9e5f489f13dd454dc
Chaz Simons <chaz.simons@gmail.com>
2021-10-12 15:48:16 -0700

    Feat: add class customer view, controller, model

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,25 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def new
+  end
+
+  def create
+    customer = Customer.new({
+      first_name: params[:customer][:first_name],
+      last_name: params[:customer][:last_name],
+      address: params[:customer][:address],
+      over_21: params[:customer][:over_21],
+      orders_placed: params[:customer][:orders_placed]
+      })
+
+    customer.save
+    redirect_to '/customers'
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+
+
+  get '/customers', to: 'customers#index'
+  get '/customers/new', to: 'customers#new'
+  post '/customers', to: 'customers#create'
+  get '/customers/:id', to: 'customers#show'
 end
