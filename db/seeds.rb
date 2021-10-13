e4a98cd81a80aac3bc670217307937e782872b5d
Sam Devine <samuel.will.devine@gmail.com>
2021-10-13 15:26:00 -0600

    feat: Restaurant show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{
+    name: 'Denver Pizza Company',
+    liquor_license: false,
+    menu_items: 10
+},{
+    name: "City, O' City",
+    liquor_license: true,
+    menu_items: 40
+},{
+    name: "Santiago's",
+    liquor_license: false,
+    menu_items: 10
+}])
+
+employees = Employee.create({
+    first_name: 'Michael',
+    last_name: 'Bluth',
+    currently_employed: true,
+    wage: 25.00,
+    restaurant_id: 1
+},{
+    first_name: 'Michael',
+    last_name: 'Scott',
+    currently_employed: false,
+    wage: 20.00,
+    restaurant_id: 2
+},{
+    first_name: 'Michael',
+    last_name: 'Jordan',
+    currently_employed: true,
+    wage: 45.00,
+    restaurant_id: 3
+})
