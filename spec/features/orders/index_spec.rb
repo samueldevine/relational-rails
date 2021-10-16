b5d79d29f29f4b07fcbcc992c0a0a6c18821b8fa
Chaz Simons <chaz.simons@gmail.com>
2021-10-15 17:21:13 -0700

    Add where boolean value is true filter to order index

--- spec/features/orders/index_spec.rb
+++ spec/features/orders/index_spec.rb
@@ -2,28 +2,79 @@ require 'rails_helper'
 
 RSpec.describe 'orders index page', type: :feature do
   it 'can show a index of all orders' do
-    order_1 = Order.create({
+    customer_1 = Customer.create({
+      first_name: 'Chaz',
+      last_name: 'Simons',
+      address: '1234 Cool Guy Lane',
+      over_21: true,
+      rewards: 1000
+      })
+    customer_2 = Customer.create({
+      first_name: 'Roger',
+      last_name: 'Smith',
+      address: '123 Cherry St',
+      over_21: true,
+      rewards: 100
+      })
+    order_1 = customer_1.orders.create({
       special_instructions: 'extra olives',
       paid: true,
       number_of_items: 5,
       })
-    order_2 = Order.create({
+    order_2 = customer_2.orders.create({
       special_instructions: 'bake with love',
       paid: true,
       number_of_items: 2,
       })
-    order_3 = Order.create({
+    order_3 = customer_2.orders.create({
       special_instructions: 'no gluten please',
       paid: false,
       number_of_items: 4,
       })
 
-
     visit '/orders'
 
-    expect(page).to have_content(order_1.id)
-    expect(page).to have_content(order_2.id)
-    expect(page).to have_content(order_3.id)
+    expect(page).to have_content("extra olives")
+    expect(page).to have_content("bake with love")
+    expect(page).to_not have_content("no gluten please")
     expect(page).to_not have_content('No olives please')
   end
+
+  it 'displays only the orders that are paid' do
+    customer_1 = Customer.create({
+      first_name: 'Chaz',
+      last_name: 'Simons',
+      address: '1234 Cool Guy Lane',
+      over_21: true,
+      rewards: 1000
+      })
+    customer_2 = Customer.create({
+      first_name: 'Roger',
+      last_name: 'Smith',
+      address: '123 Cherry St',
+      over_21: true,
+      rewards: 100
+      })
+    order_1 = customer_1.orders.create({
+      special_instructions: 'extra olives',
+      paid: true,
+      number_of_items: 5,
+      })
+    order_2 = customer_2.orders.create({
+      special_instructions: 'bake with love',
+      paid: true,
+      number_of_items: 2,
+      })
+    order_3 = customer_2.orders.create({
+      special_instructions: 'no gluten please',
+      paid: false,
+      number_of_items: 4,
+      })
+
+      visit '/orders'
+
+      expect(page).to have_content('extra olives')
+      expect(page).to have_content('bake with love')
+      expect(page).to_not have_content('no gluten please')
+    end
 end
