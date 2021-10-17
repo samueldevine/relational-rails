32e2bc38fe162fe362457ace8c3187db89e84582
Sam Devine <samuel.will.devine@gmail.com>
2021-10-17 14:09:20 -0600

    Add appear_before assertion to rails_helper

--- spec/features/restaurants/employees/index_spec.rb
+++ spec/features/restaurants/employees/index_spec.rb
@@ -16,7 +16,19 @@ RSpec.describe "Restaurant's employee index" do
     @ee2 = @restaurant.employees.create!(
       first_name: 'Michael',
       last_name: 'Jordan',
-      currently_employed: false,
+      currently_employed: true,
+      wage: 45.00
+    )
+    @ee3 = @restaurant.employees.create!(
+      first_name: 'Michael',
+      last_name: 'A',
+      currently_employed: true,
+      wage: 45.00
+    )
+    @ee4 = @restaurant.employees.create!(
+      first_name: 'Michael',
+      last_name: 'Z',
+      currently_employed: true,
       wage: 45.00
     )
   end
@@ -34,5 +46,20 @@ RSpec.describe "Restaurant's employee index" do
     click_on @ee1.last_name
 
     expect(current_path).to eq("/employees/#{@ee1.id}")
+  end
+
+  it 'can sort employees alphabetically' do
+    visit "restaurants/#{@restaurant.id}/employees"
+    expect(@ee1.first_name + ' ' + @ee1.last_name).to appear_before(@ee2.first_name + ' ' + @ee2.last_name)
+    expect(@ee2.first_name + ' ' + @ee2.last_name).to appear_before(@ee3.first_name + ' ' + @ee3.last_name)
+    expect(@ee3.first_name + ' ' + @ee3.last_name).to appear_before(@ee4.first_name + ' ' + @ee4.last_name)
+    expect(page).to have_content "Sort Employees Alphabetically"
+
+    click_on "Sort Employees Alphabetically"
+
+    expect(current_path).to eq "/restaurants/#{@restaurant.id}/employees/alpha_sorted"
+    expect(@ee3.first_name + ' ' + @ee3.last_name).to appear_before(@ee2.first_name + ' ' + @ee2.last_name)
+    expect(@ee2.first_name + ' ' + @ee2.last_name).to appear_before(@ee1.first_name + ' ' + @ee1.last_name)
+    expect(@ee3.first_name + ' ' + @ee3.last_name).to appear_before(@ee4.first_name + ' ' + @ee4.last_name)
   end
 end
